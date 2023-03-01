b8da5868ac1b04b0bd9d84b2f2ac65e4ecababf7
曾梓浩 <86450541+2002ChiJi@users.noreply.github.com>
2023-03-01 11:18:31 +0800

    0.0.3

---
+++ GPA-202303011118/app.js
@@ -0,0 +1,9 @@
+App({
+  onLaunch: function () {
+
+  },
+  globalData :{
+    scores: [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    credits:[0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  }
+})
---
+++ GPA-202303011118/index/index.js
@@ -0,0 +1,36 @@
+Page({
+  data: {
+    gpa: 0,
+  },
+  bindKeyInput: function (e) {
+
+      const app=getApp();
+      const scores=app.globalData.scores;
+      const credits=app.globalData.credits;
+      const name=e.currentTarget.dataset.name;
+      const index=Number(name[name.length-1]);
+
+      if(name[0]=='c')
+         credits[index]=Number(e.detail.value);
+      else{
+         var score=Number(e.detail.value);
+         scores[index]=score<60?0:(score-50)/10;
+      }
+  },
+  calc: function (e) {
+
+    const app=getApp();
+    const scores=app.globalData.scores;
+    const credits=app.globalData.credits;
+    var creSum=0,scoreSum=0;
+
+    for(var i=0,len=scores.length;i<len;i++)
+    {
+        creSum+=credits[i];
+        scoreSum+=(scores[i]*credits[i]);
+    }
+    this.setData({
+        gpa: scoreSum/creSum
+      })
+    }
+})
